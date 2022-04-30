85f066e2743ca9d898f6e8a140510ddc2ecef512
bugrahanceran <bugrahanmd@gmail.com>
2022-04-30 18:42:02 +0300

    sonar-project.properties added

---
+++ var/cache/test/url_matching_routes.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This file has been auto-generated
+ * by the Symfony Routing Component.
+ */
+
+return [
+    false, // $matchHost
+    [ // $staticRoutes
+    ],
+    [ // $regexpList
+        0 => '{^(?'
+                .'|/(ar|en|fr|de|es|cs|nl|ru|uk|ro|pt_BR|pl|it|ja|id|ca|sl|hr|zh_CN|bg|tr|lt|bs|sr_Cyrl|sr_Latn)?(*:101)'
+            .')/?$}sDu',
+    ],
+    [ // $dynamicRoutes
+        101 => [
+            [['_route' => 'homepage', 'template' => 'default/homepage.html.twig', '_locale' => 'en', '_controller' => 'Symfony\\Bundle\\FrameworkBundle\\Controller\\TemplateController::templateAction'], ['_locale'], null, null, false, true, null],
+            [null, null, null, null, false, false, 0],
+        ],
+    ],
+    null, // $checkCondition
+];
